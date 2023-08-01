d5393d221ab73b146a1c3a52140341e3542177d1
Allen DeMoura <allen.demoura@gmail.com>
2023-07-31 19:37:13 -0700

    dashboard rework

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -1,41 +1,38 @@
 import { useLoaderData } from "react-router-dom";
 import { UserButton, useUser } from "@clerk/clerk-react";
 
-//  auth spoof placeholder
-const me = {
-  firstName: "Tebbo",
-  lastName: "the dark lord",
-  id: "darkness",
-};
+// TODO: look into global state for logged in user?
 
 export const dashboardLoader = async ({ params }) => {
-  const [users, pools, myBets] = await Promise.all([
+  const [users, pools, bets] = await Promise.all([
     fetch(`${process.env.REACT_APP_API_SERVER_URL}/users`).then((res) => res.json()),
     fetch(`${process.env.REACT_APP_API_SERVER_URL}/pools/active`).then((res) => res.json()),
-    // TODO: use real auth instead of spoof for this (need global state?)
-    fetch(`${process.env.REACT_APP_API_SERVER_URL}/users/${me.id}/bets`).then((res) => res.json()),
+    fetch(`${process.env.REACT_APP_API_SERVER_URL}/bets`).then((res) => res.json()),
   ]);
 
   // sort users by balance
   users.sort((a, b) => b.balance - a.balance);
 
-  return { users, pools, myBets };
+  return { users, pools, bets };
 };
 
 export function Dashboard() {
-  const { users, pools, myBets } = useLoaderData();
+  const { users, pools, bets } = useLoaderData();
 
   const currentUser = useUser();
-
-  // if logged in, store logged in user in variable and add balance to it
+  // if logged in, store logged in user in variable and find their bets
   let loggedInUser = null;
+  let myBets = null;
+  let activeBets = null;
+  let closedBets = null;
   if (currentUser.isLoaded && currentUser.isSignedIn) {
-    loggedInUser = users.filter((user) => user.id === currentUser.id)[0];
+    loggedInUser = currentUser.user;
+    myBets = bets.filter((bet) => bet.better.id === loggedInUser.id);
+    activeBets = myBets.filter((bet) => bet.pool.result === "PENDING");
+    closedBets = myBets.filter((bet) => bet.pool.result !== "PENDING");
   }
 
-  // html to be rendered in browser
   return (
-    // mobile optimized tailwind css
     <>
       <header className="p-4 text-2xl font-bold">Gambol!</header>
 
@@ -46,63 +43,45 @@ export function Dashboard() {
       {/* user list */}
       <div className="border-b border-gray-400 p-4">
         <div className="font-bold">Leaderboard</div>
-        {
-          // loop through users and render them
-          users.map((user) => (
-            <div key={user.id}>
-              {user.firstName} {user.lastName}: {user.balance}
-            </div>
-          ))
-        }
+        {users.map((user) => (
+          <div key={user.id}>
+            {user.firstName} {user.lastName}: {user.balance}
+          </div>
+        ))}
       </div>
 
-      {/* list of my bets TODO: change this to active bets*/}
+      {/* list of active bets */}
       <div className="border-b border-gray-400 p-4">
-        <div className="font-bold">My Bets</div>
-        {
-          // loop through bets and render them
-          myBets.map &&
-            myBets.map((bet) => (
-              <div key={bet.id}>
-                {bet.poolID} {bet.amount} {bet.bet}
-              </div>
-            ))
-        }
+        <div className="font-bold">Your Active Bets</div>
+        {activeBets.map &&
+          activeBets.map((bet) => (
+            <div key={bet.id}>
+              {bet.pool.desc} {bet.bet} {bet.amount}
+            </div>
+          ))}
+      </div>
+
+      {/* history of closed bets */}
+      <div className="border-b border-gray-400 p-4">
+        <div className="font-bold">Your Closed Bets (History)</div>
+        {closedBets.map &&
+          closedBets.map((bet) => (
+            <div key={bet.id}>
+              {bet.pool.desc} {bet.bet === bet.pool.result ? "WON" : "LOST"} {bet.amount}
+            </div>
+          ))}
       </div>
 
       {/* list of all active pools */}
       <div className="border-b border-gray-400 p-4">
-        <div className="font-bold">All Pools</div>
-        {
-          // loop through pools and render them
-          pools.map((pool) => (
-            <div key={pool.id}>
-              <a href={`/pool/${pool.id}`} className="underline text-blue-400" key={pool.id}>
-                {pool.desc} <b>{pool.point}</b> Under: {pool.underPool} Over: {pool.overPool}
-                {/* generate form for betting on each pool */}
-                {/* <form onSubmit={handleSubmit} className="mb-8">
-                  <div className="border-b border-gray-400 mb-2">
-                    <input type="number" name="amount" placeholder="Amount" />
-                  </div>
-
-                  <div className="border-b border-gray-400 mb-2">
-                    <select name="bet">
-                      <option value="OVER">Over</option>
-                      <option value="UNDER">Under</option>
-                    </select>
-                  </div>
-
-                  <input type="hidden" name="poolID" value={pool.id} />
-                  <input type="hidden" name="better" value={me.name} />
-
-                  <button type="submit" className="bg-blue-700 rounded-sm px-4 py-2 text-white" value="Submit">
-                    Submit
-                  </button>
-                </form> */}
-              </a>
-            </div>
-          ))
-        }
+        <div className="font-bold">Active Pools</div>
+        {pools.map((pool) => (
+          <div key={pool.id}>
+            <a href={`/pool/${pool.id}`} className="underline text-blue-400" key={pool.id}>
+              {pool.desc} <b>{pool.point}</b> Under: {pool.underPool} Over: {pool.overPool}
+            </a>
+          </div>
+        ))}
       </div>
     </>
   );
